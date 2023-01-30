924eda36afafeb45e121925e7958d2b94b0dfd9f
송관재 <kosdjs39@gmail.com>
2023-01-30 17:25:55 +0900

    Feat: [S08P12D207-228] 방 퇴장시 창 UI 구현

--- Android/app/src/main/java/com/hanjan/hanjangame/GameRoomActivity.kt
+++ Android/app/src/main/java/com/hanjan/hanjangame/GameRoomActivity.kt
@@ -1,5 +1,6 @@
 package com.hanjan.hanjangame
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.hanjan.hanjangame.adapter.UserListAdapter
 import com.hanjan.hanjangame.adapter.showGameListDialog
 import com.hanjan.hanjangame.databinding.ActivityGameRoomBinding
+import com.hanjan.hanjangame.databinding.GameExitDialogBinding
 import com.hanjan.hanjangame.dto.UserWithData
 
 class GameRoomActivity : AppCompatActivity() {
@@ -26,11 +28,32 @@ class GameRoomActivity : AppCompatActivity() {
         binding.gameStartBtn.setOnClickListener {
             startActivity(Intent(this, GameListActivity::class.java))
         }
+        binding.backButton.setOnClickListener {
+            showExitDialog()
+        }
         binding.gameRoomListBtn.setOnClickListener {
             showGameListDialog(this)
         }
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = UserListAdapter(userList)
         binding.recyclerView.suppressLayout(true)
+    }
+
+    override fun onBackPressed() {
+        showExitDialog()
+    }
+
+    fun showExitDialog(){
+        val builder = AlertDialog.Builder(this)
+        val dialogBinding = GameExitDialogBinding.inflate(layoutInflater)
+        builder.setView(dialogBinding.root)
+        val dialog = builder.show()
+        dialogBinding.dialogExitBtn.setOnClickListener {
+            dialog.dismiss()
+            finish()
+        }
+        dialogBinding.dialogCancelBtn.setOnClickListener {
+            dialog.dismiss()
+        }
     }
 }

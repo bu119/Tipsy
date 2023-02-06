b87491921e2b01856c98d74ad64387a80788c65e
kimbugyeong <sa01023@naver.com>
2023-02-06 14:58:33 +0900

    feat: [S08P12D207-343] Phaser 컴포넌트화

---
+++ FE/react-phaser3 Custom/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import GameView from "./pages/GameView";
+import About from "./pages/About";
+
+// export default class App extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Hello World</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/phaser' element={<GameView/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

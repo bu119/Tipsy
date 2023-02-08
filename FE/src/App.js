d9746494892590f0b05bef699a4ff6b445e823f7
boham97 <kangbosung97@gmail.com>
2023-02-09 00:11:17 +0900

    Extra: 프론트 파일 통합

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,21 +1,36 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
+import { CookiesProvider } from 'react-cookie';
+
+//import Home from "./pages/Home";
 import GameView from "./pages/GameView";
 import MainView from "./pages/MainView";
 import About from "./pages/About";
 import QRcode from "./components/QRcode";
+
+import './App.css';
+import Preview from './pages/Preview';
+import Login from './pages/Login';
+import Mypage from './pages/Mypage';
+import Temp from './pages/Temp';
+import Map from './pages/Map';
+import Meeting from './pages/Meeting';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/mainstreet' element={<MainView/>}/>
           <Route path='/phaser' element={<GameView/>}/>
           <Route path='/QR' element={<QRcode/>}/>
+          <Route path='/' element={<Preview/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/mypage' element={<Mypage/>}/>
+          <Route path='/temp' element= {<Temp/>}/>
+          <Route path='/map' element= {<Map/>}/>
+          <Route path='/meeting' element= {<Meeting/>}/>
         </Routes>
       </div>
     </BrowserRouter>
